1350a53a28bc34ac1e1dd145f13e2adc0936d5b3
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-27 20:45:24 +0100

    Add application skeleton

---
+++ src/arcana-linter/file_util.hh
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <filesystem> // TODO cc::filesystem
+#include <iostream>
+#include <string> // TODO cc::string
+
+namespace fs = std::filesystem;
+
+namespace arclint
+{
+/// Calls func with all paths at or within the given one, returns false iff no regular files are found
+template <class F>
+bool iterate_regular_files(std::string const& root, F func)
+{
+    auto const path = fs::path(root);
+    if (fs::is_directory(path))
+    {
+        for (auto const& p : fs::recursive_directory_iterator(path))
+        {
+            if (p.is_regular_file())
+            {
+                func(p.path());
+            }
+        }
+
+        return true;
+    }
+    else if (fs::is_regular_file(path))
+    {
+        func(path);
+        return true;
+    }
+    return false;
+}
+
+/// Returns true if the file's extension indicates that it is a C++/C file
+[[nodiscard]] inline bool has_cpp_file_extension(fs::path const& path)
+{
+    auto const cpp_file_exts = {".cpp", ".cxx", ".c", ".cc", ".hpp", ".h", ".hxx", ".hh", ".inl"};
+    auto const ext = path.extension().string();
+
+    for (auto const& cpp_ext : cpp_file_exts)
+        if (ext.compare(cpp_ext) == 0)
+            return true;
+
+
+    return false;
+}
+
+/// Returns true if the file's extension is canonical (cc/hh)
+[[nodiscard]] inline bool has_canonical_cpp_file_extension(fs::path const& path)
+{
+    auto const canonical_cpp_file_exts = {".cc", ".hh"};
+    auto const ext = path.extension().string();
+
+    for (auto const& cpp_ext : canonical_cpp_file_exts)
+        if (ext.compare(cpp_ext) == 0)
+            return true;
+
+    return false;
+}
+
+}
--- src/arcana-linter/main.cc
+++ src/arcana-linter/main.cc
@@ -1,2 +1,51 @@
+#include <fstream>
+#include <iostream> // TODO rlog
+#include <string>   // TODO cc::string
 
-int main() {}
+#include <cppast/cpp_file.hpp>
+#include <cppast/libclang_parser.hpp>
+
+#include "file_util.hh"
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "usage: ./arcana-linter <file|folder>" << std::endl;
+    }
+    else
+    {
+        cppast::cpp_entity_index idx;
+        cppast::stderr_diagnostic_logger logger;
+        cppast::libclang_compile_config config;
+        cppast::libclang_parser parser(type_safe::ref(logger));
+
+        auto const iterate_res = arclint::iterate_regular_files(argv[1], [&](fs::path const& file) {
+            if (arclint::has_cpp_file_extension(file))
+            {
+                std::cout << "parsing " << file.c_str() << std::endl;
+                if (!arclint::has_canonical_cpp_file_extension(file))
+                {
+                    std::cout << "warning: " << file.c_str() << " does not have canonical file extensions .cc/.hh" << std::endl;
+                }
+
+                try
+                {
+                    auto const parse_result = parser.parse(idx, file.string(), config);
+                }
+                catch (std::exception e)
+                {
+                    std::cerr << "Error parsing " << file.c_str() << ", parse exception: " << e.what() << std::endl;
+                }
+
+                if (parser.error())
+                {
+                    std::cerr << "Error parsing " << file.c_str() << std::endl;
+                }
+            }
+        });
+
+        if (!iterate_res)
+            std::cerr << "Failed to open " << argv[1] << std::endl;
+    }
+}
